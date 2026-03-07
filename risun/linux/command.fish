d95ba14a6ad02b3b7de5b28e9440a42ca58ad961
risun <fengwangtsy@gmail.com>
2026-03-07 22:00:18 +0800

    feat: add backup functions for Arknights and Endfield games using rsync

--- risun/linux/command.fish
+++ risun/linux/command.fish
@@ -25,9 +25,34 @@ end
 
 function rsync_copy_incr
     if test (count $argv) -lt 2
-        echo "Usage: rsync_copy_incr <source> <destination>"
+        printf "%s\n" "Usage: rsync_copy_incr <source> <destination>"
         return 1
     end
     rsync -av --update --delete $argv[1]/ $argv[2]/
 end
 
+function rsync_backup_arknights
+    set folder (find ~/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/compatdata/ -type d -name "Arknights Game" -print -quit)
+    set destination /mnt/kingston_nv3/games/hypergryph/arknights
+
+    if test -n "$folder"; and test -d "$folder"
+        mkdir -p "$destination/Arknights Game"
+        rsync_copy_incr "$folder" "$destination/Arknights Game"
+    else
+        printf "%s\n" "Arknights Game folder not found."
+        return 1
+    end
+end
+
+function rsync_backup_endfield
+    set folder (find ~/.var/app/com.valvesoftware.Steam/.local/share/Steam/steamapps/compatdata/ -type d -name "Endfield Game" -print -quit)
+    set destination /mnt/kingston_nv3/games/hypergryph/endfield
+
+    if test -n "$folder"; and test -d "$folder"
+        mkdir -p "$destination/Endfield Game"
+        rsync_copy_incr "$folder" "$destination/Endfield Game"
+    else
+        printf "%s\n" "Endfield Game folder not found."
+        return 1
+    end
+end
