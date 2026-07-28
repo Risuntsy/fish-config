a92f6468cab3c3577c797e010abba948ae589c7b
risun <fengwangtsy@gmail.com>
2026-07-29 00:17:59 +0800

    1785255479

--- risun/env/env.fish
+++ risun/env/env.fish
@@ -6,3 +6,6 @@ if test -d $HOME/.bun/bin; and not contains $HOME/.bun/bin $fish_user_paths
     fish_add_path $HOME/.bun/bin
 end
 
+if test -d $HOME/.local/share/flutter/bin
+    fish_add_path -U -p $HOME/.local/share/flutter/bin
+end
