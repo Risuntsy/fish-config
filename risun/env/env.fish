fc03a948114739378029e55665eb2e950dfdf8b5
risun <fengwangtsy@gmail.com>
2026-08-03 11:00:59 +0800

    1785726059

--- risun/env/env.fish
+++ risun/env/env.fish
@@ -2,8 +2,18 @@ if command --query podman
     podman completion fish | source
 end
 
-if test -d $HOME/.bun/bin; and not contains $HOME/.bun/bin $fish_user_paths
-    fish_add_path $HOME/.bun/bin
+# bun
+set -gx BUN_INSTALL "$HOME/.bun"
+if test -d $BUN_INSTALL/bin; and not contains $BUN_INSTALL/bin $fish_user_paths
+    fish_add_path $BUN_INSTALL/bin
+end
+
+if test -d $HOME/.local/share/flutter/bin
+    fish_add_path -U -p $HOME/.local/share/flutter/bin
+end
+# uv / local bin
+if test -d "$HOME/.local/bin"; and not contains "$HOME/.local/bin" $fish_user_paths
+    fish_add_path "$HOME/.local/bin"
 end
 
 if test -d $HOME/.local/share/flutter/bin
